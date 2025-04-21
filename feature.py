b2ca52ca0b2629985dd3d4025d2c0056e22d7e7e
carlpang1 <carlking0088@hotmail.com>
2025-04-21 14:13:47 +0800

    Added feature.py to have function to add 2 numbers

---
+++ feature.py
@@ -0,0 +1,8 @@
+def add2numbers(num1, num2):
+    return num1 + num2
+
+if __name__ == "__main__":
+    num1 = 8
+    num2 = 3
+    result = add2numbers(num1,num2)
+    print(f"The sum of {num} and {num2} is {result}")
